f71ccca20ec0fe486c74e328a4175f6f822d57e1
Wassim Metallaoui <wassim@metallaoui.com>
2026-02-14 08:09:01 -0600

    test: use require_relative instead of shell syntax check
    Require the file directly which catches both syntax errors and
    load-time issues. The __FILE__ guard prevents execution.

--- test/run_test.rb
+++ test/run_test.rb
@@ -1,8 +1,7 @@
 require "test_helper"
 
 class RunTest < Minitest::Test
-  def test_run_file_has_valid_syntax
-    result = `bundle exec ruby -c lib/run.rb 2>&1`
-    assert_includes result, "Syntax OK", "lib/run.rb has a syntax error: #{result}"
+  def test_run_file_loads_without_error
+    assert require_relative("../lib/run")
   end
 end
