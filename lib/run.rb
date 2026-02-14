ff9277fcc71c3eaf681197ba46d66434d4bded48
Wassim Metallaoui <wassim@metallaoui.com>
2026-02-14 06:08:00 -0600

    refactor: extract NotionClient class
    Move Notion API interaction into GamingLibrary::NotionClient with
    constructor injection for api_key and database_id. Consolidates
    HTTP boilerplate, pagination, and property building into the class.
    Slim run.rb down to orchestration logic using both clients.

--- lib/run.rb
+++ lib/run.rb
@@ -1,11 +1,8 @@
 require "dotenv/load"
 require "awesome_print"
-require "net/http"
-require "json"
-require "uri"
 require "debug"
-require "date"
 require_relative "gaming_library/steam_client"
+require_relative "gaming_library/notion_client"
 
 def steam_client
   @steam_client ||= GamingLibrary::SteamClient.new(
@@ -15,83 +12,29 @@ def steam_client
   )
 end
 
-def fetch_notion_games
-  uri = URI("https://api.notion.com/v1/databases/#{ENV["NOTION_DATABASE_ID"]}/query")
-  header = {
-    Authorization: "Bearer #{ENV["NOTION_API_KEY"]}",
-    "Content-Type": "application/json",
-    "Notion-Version": "2022-06-28",
-  }
-
-  all_results = []
-  has_more = true
-  start_cursor = nil
-
-  while has_more
-    body = {}
-    body[:start_cursor] = start_cursor if start_cursor
-    body = body.to_json
-
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-    request = Net::HTTP::Post.new(uri.path, header)
-    request.body = body
-    response = http.request(request)
-    data = JSON.parse(response.body)
-
-    all_results.concat(data["results"])
-    has_more = data["has_more"]
-    start_cursor = data["next_cursor"]
-  end
-
-  all_results
+def notion_client
+  @notion_client ||= GamingLibrary::NotionClient.new(
+    api_key: ENV["NOTION_API_KEY"],
+    database_id: ENV["NOTION_DATABASE_ID"],
+  )
 end
 
-def insert_notion_database(steam_games, notion_games)
-  uri = URI("https://api.notion.com/v1/pages")
-  header = {
-    Authorization: "Bearer #{ENV["NOTION_API_KEY"]}",
-    "Content-Type": "application/json",
-    "Notion-Version": "2022-06-28",
-  }
-
-  notion_games_map =
-    notion_games.map { |page| [page["properties"]["Steam ID"]["number"], page["id"]] }.to_h
+def main
+  notion_pages = notion_client.fetch_games
+  notion_map = notion_client.games_map(notion_pages)
 
   puts "=" * 80
-  puts "Inserting into Notion database"
-  puts "Working with #{notion_games_map.count} Notion games"
-  puts "Working with #{steam_client.owned_games.count} Steam games"
+  puts "Steam games: #{steam_client.owned_games.count}"
+  puts "Notion games: #{notion_map.count}"
   puts "=" * 80
 
+  # Insert new games
   steam_client.owned_games.each do |game|
-    body = {
-      properties: {
-        Name: {
-          title: [{ text: { content: game[:name] } }],
-        },
-        "Playtime (Minutes)": {
-          number: game[:playtime_forever],
-        },
-        "Steam ID": {
-          number: game[:steam_id],
-        },
-      },
-    }
-
-    next if notion_games_map.key?(game[:steam_id]) # game already exists in Notion
-    # exclude this game
+    next if notion_map.key?(game[:steam_id])
     next if steam_client.excluded?(game)
 
-    uri = URI("https://api.notion.com/v1/pages")
-    request = Net::HTTP::Post.new(uri.path, header)
-    body[:parent] = { database_id: ENV["NOTION_DATABASE_ID"] }
-    request.body = body.to_json
-
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-    response = http.request(request)
-    if response.code == "200"
+    code = notion_client.insert_game(game)
+    if code == "200"
       puts "Added Notion entry for game: #{game[:name]} - #{game[:steam_id]}"
     else
       puts "API error adding Notion entry for game: #{game[:name]} - #{game[:steam_id]}"
@@ -100,113 +43,30 @@ def insert_notion_database(steam_games, notion_games)
     puts "Program error adding Notion entry for game: #{game[:name]} - #{game[:steam_id]}"
     puts e.message
   end
-end
 
-def update_notion_database(steam_games, notion_games)
-  header = {
-    Authorization: "Bearer #{ENV["NOTION_API_KEY"]}",
-    "Content-Type": "application/json",
-    "Notion-Version": "2022-06-28",
-  }
-
-  notion_games_map =
-    notion_games.map { |page| [page["properties"]["Steam ID"]["number"], page["id"]] }.to_h
-
-  puts "=" * 80
-  puts "Updating Notion database"
-  puts "Working with #{notion_games_map.count} Notion games"
-  puts "Working with #{steam_client.owned_games.count} Steam games"
-  puts "=" * 80
-
+  # Update existing games
   steam_client.owned_games.each do |game|
-    details = steam_client.game_details(game[:steam_id])
-
-    if details.nil?
-      puts "Game details API call for #{game[:name]} failed"
-      next
-    end
-
-    publishers = details["publishers"]
-    developers = details["developers"]
-    genres = details["genres"].map { |genre| genre["description"] }
-    release_date =
-      begin
-        Date.parse(details["release_date"]["date"].scan(/[,\w+\s]/).join)
-      rescue StandardError # some steam_games have bad dates like Warhammer 40K: Space Marine II
-        nil
-      end
-    last_played_date = game[:last_played_date]
-    icon_url = game[:icon_url]
-    logo_url = details["capsule_imagev5"]
-
-    properties = {}
-    if !publishers.nil?
-      properties[:Publishers] = {
-        multi_select: publishers.map { |publisher| { name: publisher.gsub(",", "") } },
-      }
-    end
-    if !developers.nil?
-      properties[:Developers] = {
-        multi_select: developers.map { |developer| { name: developer.gsub(",", "") } },
-      }
-    end
-    if !genres.nil?
-      properties[:Genres] = { multi_select: genres.map { |genre| { name: genre.gsub(",", "") } } }
-    end
-
-    properties["Release Date".to_sym] = { date: { start: release_date.to_s } } if !release_date.nil?
-
-    if !icon_url.nil?
-      properties["Icon".to_sym] = {
-        files: [
-          { name: "logo", type: "external", external: { url: logo_url } },
-        ],
-      }
-    end
-
-    if !last_played_date.nil?
-      properties["Last Played Date".to_sym] = { date: { start: last_played_date.to_date.to_s } }
-    end
-
-    properties["Playtime (Minutes)".to_sym] = { number: game[:playtime_forever] }
-
-    body = { properties: properties }.to_json
-
-    page_id = notion_games_map[game[:steam_id]]
-
     next if steam_client.excluded?(game)
 
+    page_id = notion_map[game[:steam_id]]
     if page_id.nil?
       puts "Game does not exist in Notion: #{game[:name]}"
       next
     end
 
-    uri = URI("https://api.notion.com/v1/pages/#{page_id}")
-    request = Net::HTTP::Patch.new(uri.path, header)
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-    request.body = body
-    response = http.request(request)
-
-    if response.code == "200"
-      puts "Updated Notion for game: #{game[:name]} - #{game[:steam_id]}"
-    else
-      puts uri
-      puts JSON.parse(request.body)
-      puts "API error updating Notion for game: #{game[:name]} - #{game[:steam_id]}"
-      puts response.body
+    details = steam_client.game_details(game[:steam_id])
+    if details.nil?
+      puts "Game details API call for #{game[:name]} failed"
+      next
     end
+
+    notion_client.update_game(page_id: page_id, game: game, details: details)
+    puts "Updated Notion for game: #{game[:name]} - #{game[:steam_id]}"
     sleep 1
   rescue StandardError => e
     puts "Program error updating Notion for game: #{game[:name]} - #{game[:steam_id]}"
     puts e.message
   end
-end
-
-def main
-  notion_response = fetch_notion_games
-  insert_notion_database(steam_client.owned_games, notion_response)
-  update_notion_database(steam_client.owned_games, notion_response)
 end
 
 main if __FILE__ == $PROGRAM_NAME
